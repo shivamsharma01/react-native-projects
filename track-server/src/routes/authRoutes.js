34372e2b860b4447135446eac825dd74c317bef2
Shivam Sharma <shivamsharma.ade@gmail.com>
2023-02-12 17:26:23 +0530

    add all projects

---
+++ track-server/src/routes/authRoutes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+
+const User = mongoose.model("User");
+
+const router = express.Router();
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = new User({ email, password });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).send({ error: "Must provide email and password" });
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).send({ error: "Email not found" });
+  }
+  try {
+    user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    return res.send({ token });
+  } catch (err) {
+    return res.status(422).send({ error: "Invalid password" });
+  }
+});
+
+module.exports = router;
